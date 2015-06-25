cbe920ceccaf14b4340034f411f3478fac6bc263
Travis CI <support@perfectsensedigital.com>
2015-06-25 15:29:36 -0400

    Adds GitHub release tagging.

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,17 @@ if [[ "$TRAVIS_REPO_SLUG" == "perfectsense/"* ]] && \
   if [[ "$TRAVIS_BRANCH" == "release/"* ]]; then
 
     echo "Preparing RELEASE version..."
+    git fetch --unshallow || true
     mvn -Pprepare-release initialize
+    VERSION="v$(cat newVersion)"
 
     echo "Deploying RELEASE to Maven repository..."
     mvn clean $OPTIONS
+
+    echo "Tagging RELEASE in GitHub..."
+    git config --global user.email "support@perfectsensedigital.com"
+    git config --global user.name "Travis CI"
+    git tag $VERSION -a -m "$VERSION"
+    git push -q "https://$GITHUB_AUTHORIZATION@github.com/$TRAVIS_REPO_SLUG" $VERSION > /dev/null 2>&1
   fi
 fi
