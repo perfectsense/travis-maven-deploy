b3148a5bdb82f7d37bb8e080c7b7884edf362360
Hyoo Lim <hyoolim@gmail.com>
2015-07-20 16:16:41 -0400

    Removes auto release tagging.

--- deploy.sh
+++ deploy.sh
@@ -19,15 +19,8 @@ if [[ "$TRAVIS_REPO_SLUG" == "perfectsense/"* ]] && \
     echo "Preparing RELEASE version..."
     git fetch --unshallow || true
     mvn -Pprepare-release initialize
-    VERSION="v$(cat newVersion)"
 
     echo "Deploying RELEASE to Maven repository..."
     mvn clean $OPTIONS
-
-    echo "Tagging RELEASE in GitHub..."
-    git config --global user.email "support@perfectsensedigital.com"
-    git config --global user.name "Travis CI"
-    git tag $VERSION -a -m "$VERSION"
-    git push -q "https://$GITHUB_AUTHORIZATION@github.com/$TRAVIS_REPO_SLUG" $VERSION > /dev/null 2>&1
   fi
 fi
