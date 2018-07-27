82767d287a4d22ccd6416f14497359915c9e89eb
Dan Slaughter <dan@perfectsensedigital.com>
2018-07-27 09:40:51 -0400

    Skip tests on on tag builds since we should only be tagging from a commit that has already been successfully built and passed tests.

--- brightspot-deploy.rb
+++ brightspot-deploy.rb
@@ -746,6 +746,7 @@ def deploy
 
       system_stdout('mvn clean install'\
             ' -B'\
+            ' -Dmaven.test.skip=true'\
             ' -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn'\
             ' -Plibrary')
 
